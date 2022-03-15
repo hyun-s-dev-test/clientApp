f0530a151c769dbb90167e2b8498009dcc7c2f0d
doosanehi <bds0203@gmail.com>
2022-03-15 16:07:57 +0900

    redux store

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit';
+import userAgreementReducer from '../state/userAgreementSlice';
+import userAuthReducer from '../state/userAuthSlice';
+import userInfoReducer from '../state/userInfoSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    userAgreement: userAgreementReducer,
+    userAuth: userAuthReducer,
+    userInfo: userInfoReducer,
+  },
 });
